2dd7f6efa3ccec921b925df873f553d993b7a5ac
TranPhanPhucAn <21120407@student.hcmus.edu.vn>
2024-11-18 23:49:34 +0700

    exception

--- src/main/java/com/phucan/identity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/phucan/identity_service/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.phucan.identity_service.exception;
 
+import com.phucan.identity_service.dto.request.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,9 +8,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(value = RuntimeException.class)
-    ResponseEntity<String> handlingRuntimeException(RuntimeException exception){
-        return ResponseEntity.badRequest().body(exception.getMessage());
+    @ExceptionHandler(value = Exception.class)
+    ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception){
+        ApiResponse apiResponse = new ApiResponse();
+
+        apiResponse.setCode(9999);
+        apiResponse.setMessage(exception.getMessage());
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<ApiResponse> handlingAppException(AppException exception){
+        ErrorCode errorCode = exception.getErrorCode();
+        ApiResponse apiResponse = new ApiResponse();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+        return ResponseEntity.badRequest().body(apiResponse);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
