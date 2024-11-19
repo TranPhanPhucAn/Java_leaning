0c31e22ecd616900da6f9902ea6a665a5b1b4551
TranPhanPhucAn <21120407@student.hcmus.edu.vn>
2024-11-19 23:45:14 +0700

    jwt

--- src/main/java/com/phucan/identity_service/exception/ErrorCode.java
+++ src/main/java/com/phucan/identity_service/exception/ErrorCode.java
@@ -6,7 +6,10 @@ public enum ErrorCode {
 
     USER_EXISTED(1002,"User existed"),
     USERNAME_INVALID(1003,"Username must be at least 3 characters "),
-    PASSWORD_INVALID(1004,"Password must be at least 8 characters ")
+    PASSWORD_INVALID(1004,"Password must be at least 8 characters "),
+    USER_NOT_EXISTED(1005,"User not exist"),
+
+    UNAUTHENTICATED(1006,"Unauthenticated"),
 
     ;
 
---
+++ src/main/java/com/phucan/identity_service/service/AuthenticationService.java
@@ -0,0 +1,79 @@
+package com.phucan.identity_service.service;
+
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.phucan.identity_service.dto.request.AuthenticationRequest;
+import com.phucan.identity_service.dto.response.AuthenticationResponse;
+import com.phucan.identity_service.exception.AppException;
+import com.phucan.identity_service.exception.ErrorCode;
+import com.phucan.identity_service.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationService {
+    UserRepository userRepository;
+
+    @NonFinal
+    protected static final String SIGNER_KEY =
+            "5qiuk4pnYdZoMr5KMTheVfRN8ZkF7vrWglQavuAYKZXH2iHaD2q3Kh0iIYkWq316";
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request){
+        var user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(()-> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        boolean authenticated = passwordEncoder.matches(request.getPassword(), user.getPassword());
+
+        if(!authenticated){
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+        }
+
+        var token = generate(request.getUsername());
+
+        return AuthenticationResponse.builder()
+                .token(token)
+                .authenticated(true)
+                .build();
+
+    }
+    private String generate(String username){
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("phucan.com")
+                .issueTime(new Date())
+                .expirationTime(new Date(
+                        Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
+                ))
+                .build();
+
+        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        try{
+            jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+            return jwsObject.serialize();
+        }catch (JOSEException e){
+            log.error("Can not create token: ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+}
